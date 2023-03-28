510cc18e4a09141cef465378e02a12b9ef51c309
Edleron <Edleron@hotmail.com>
2023-03-28 19:36:36 +0300

    add : some changes fix : player boundries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,38 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 5.0f;
     private Vector2 rawInput;
 
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    private int paddingLeft = 1;
+    private int paddingRight = 1;
+    private int paddingTop = 5;
+    private int paddingBottom = 2;
+
+    private void Start()
+    {
+        InitBounds();
+    }
+
     private void Update()
     {
         Move();
     }
 
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void Move()
     {
         Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
