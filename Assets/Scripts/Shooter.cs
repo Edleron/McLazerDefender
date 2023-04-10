79bd63e1d455ba06edb94821c207f243c2d77c18
Edleron <Edleron@hotmail.com>
2023-04-11 01:27:01 +0300

    add : shooter system

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10.0f;
+    [SerializeField] float projectileLifeTime = 5.0f;
+    [SerializeField] float firingRate = 0.2f;
+
+    public bool isFiring;
+
+    private Coroutine firingCoroutine;
+
+    private void Update()
+    {
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (isFiring && firingCoroutine == null)
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        else if (!isFiring && firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject instance = Instantiate(projectilePrefab,
+                                                transform.position,
+                                                Quaternion.identity);
+
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }
+
+            Destroy(instance, projectileLifeTime);
+            yield return new WaitForSeconds(firingRate);
+        }
+    }
+}
