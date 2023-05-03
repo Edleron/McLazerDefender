181688e023b1706b7f0cb41efd081ba15b439854
Edleron <Edleron@hotmail.com>
2023-05-03 21:27:40 +0300

    fix : audip player script modified

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -18,6 +18,34 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] AudioClip damageClip;
     [SerializeField][Range(0f, 1f)] float damageVolume = 1.0f;
 
+    private static AudioPlayer instance;
+
+    public AudioPlayer GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        ManageSingleton();
+    }
+
+    private void ManageSingleton()
+    {
+        // int instanceCount = FindObjectsOfType(GetType()).Length;
+        // if (instanceCount > 1)
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public void PlayHeroShootingClip()
     {
         PlayClip(heroShootingClip, heroShootingVolume);
